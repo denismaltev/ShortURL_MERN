68722103e5fe29905fc947427e3863fff372b6df
denismaltev <test08081979@gmail.com>
2020-09-27 22:38:18 -0700

    MongoDB connected. .ENV connected

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
-const config = require("config");
-const PORT = config.get("port") || 5000;
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}`);
-});
+// Environment variables
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(process.env.DATABASE_ATLAS_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Server error: ", error.message);
+    process.exit(1);
+  }
+}
+
+start();
